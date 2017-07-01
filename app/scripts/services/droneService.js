c89748b5fdeecff1f0ac3b18b9998b40568459a6
Lester Thomas <lesterthomas@hotmail.com>
2017-07-01 11:49:28 +0100

    Applied droneService to individual page so that only droneService is polling for data

--- app/scripts/services/droneService.js
+++ app/scripts/services/droneService.js
@@ -2,8 +2,8 @@ angular.module('droneFrontendApp')
 .service('droneService',['$http','$interval', function($http,$interval) {
 	var self=this;
     this.droneId=-1;
-    this.apiURL='http://localhost:1235/';//'HTTP://droneapi.ddns.net:1235/'
-    this.consoleRootURL='http://localhost:1235/static/app';//'HTTP://droneapi.ddns.net:1235/static/app'
+    this.apiURL='HTTP://droneapi.ddns.net:1235/'
+    this.consoleRootURL='HTTP://droneapi.ddns.net:1235/static/app'
     this.droneName='';
     this.drones={"collection":[]};
 
