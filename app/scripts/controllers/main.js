4d0a3e43fe4278692ab0f8c1bcbe407690276a07
Lester Thomas <lesterthomas@hotmail.com>
2017-09-06 17:43:11 +0100

    Added user server call

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,13 +9,35 @@
  */
 
 angular.module('droneFrontendApp')
-  .controller('MainCtrl', ['$scope','$rootScope', '$location','$interval','facebookUser',function ($scope,$rootScope,$location,$interval,facebookUser) {
+  .controller('MainCtrl', ['$scope','$rootScope', '$location','$interval','$http','facebookUser','droneService',function ($scope,$rootScope,$location,$interval,$http,facebookUser,droneService) {
 
   	console.log('Started Main controller');
+    $scope.apiURL=droneService.apiURL;
+
     $rootScope.$on('fbLoginSuccess', function(name, response) {
       facebookUser.then(function(user) {
         user.api('/me',{fields:['name','email']}).then(function(response) {
-          $rootScope.loggedInUser = response;
+            $rootScope.loggedInUser = response;
+
+            //add to droneAPIServer user database to get API Key
+            var payload={"name":response.name,"email":response.email,"id":response.id,"id_provider":"Facebook"};
+            $http.post($scope.apiURL + 'user',payload,{
+                headers : {
+                    'Content-Type' : 'application/json; charset=UTF-8'
+                }
+            })
+            .then(function(data, status, headers, config) {
+                    var api_key=data.data.api_key;
+                    $rootScope.loggedInUser.api_key=api_key;
+                    console.log('API  user POST success',data,status);
+
+                },
+                function(data, status, headers, config) {
+                    // log error
+                    console.log('API user POST error',data, status, headers, config);
+                });
+
+
         });
       });
     });
